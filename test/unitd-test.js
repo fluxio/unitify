a6c189dc8e5990ae67b0d4a2852131efa166481f
Scott Andrews <scott@vannevartech.com>
2013-12-12 23:19:07 -0800

    Units, finally...
    Simple support for units and conversion between units. Automatic
    conversion occurs between compatible units. Operations with incompatible
    units will throw.
    
    Addition and subtraction return sane results. Multiplication and division
    contain wrong units:
    1 meter * 1 meter = 1 meter (wrong)

--- test/unitd-test.js
+++ test/unitd-test.js
@@ -8,7 +8,7 @@
 (function (buster, define) {
 	'use strict';
 
-	var assert, refute, fail;
+	var assert, refute, fail, undef;
 
 	assert = buster.assertions.assert;
 	refute = buster.assertions.refute;
@@ -46,57 +46,57 @@
 			},
 			'Measure': {
 				'raw': function () {
-					assert.same(new unitd.Measure(3).raw(), 3);
+					assert.same(new unitd.Measure(3).raw, 3);
 				},
 				'precision': function () {
-					assert.same(new unitd.Measure(3, 2).precision(), 2);
+					assert.same(new unitd.Measure(3, undef, 2).precision, 2);
 
 					// precision is bounded between 1 and 21
-					assert.same(new unitd.Measure(3).precision(), 21);
-					assert.same(new unitd.Measure(3, 100).precision(), 21);
-					assert.same(new unitd.Measure(3, -100).precision(), 1);
+					assert.same(new unitd.Measure(3).precision, 15);
+					assert.same(new unitd.Measure(3, undef, 100).precision, 15);
+					assert.same(new unitd.Measure(3, undef, -100).precision, 1);
 				},
 				'val': function () {
-					assert.same(new unitd.Measure(987654321.01234,  1).val(), 1000000000);
-					assert.same(new unitd.Measure(987654321.01234,  2).val(), 990000000);
-					assert.same(new unitd.Measure(987654321.01234,  3).val(), 988000000);
-					assert.same(new unitd.Measure(987654321.01234,  4).val(), 987700000);
-					assert.same(new unitd.Measure(987654321.01234,  5).val(), 987650000);
-					assert.same(new unitd.Measure(987654321.01234,  6).val(), 987654000);
-					assert.same(new unitd.Measure(987654321.01234,  7).val(), 987654300);
-					assert.same(new unitd.Measure(987654321.01234,  8).val(), 987654320);
-					assert.same(new unitd.Measure(987654321.01234,  9).val(), 987654321);
-					assert.same(new unitd.Measure(987654321.01234, 10).val(), 987654321.0);
-					assert.same(new unitd.Measure(987654321.01234, 11).val(), 987654321.01);
-					assert.same(new unitd.Measure(987654321.01234, 12).val(), 987654321.012);
-					assert.same(new unitd.Measure(987654321.01234, 13).val(), 987654321.0123);
-					assert.same(new unitd.Measure(987654321.01234, 14).val(), 987654321.01234);
-					assert.same(new unitd.Measure(987654321.01234, 15).val(), 987654321.01234);
+					assert.same(new unitd.Measure(987654321.01234, undef,  1).val, 1000000000);
+					assert.same(new unitd.Measure(987654321.01234, undef,  2).val, 990000000);
+					assert.same(new unitd.Measure(987654321.01234, undef,  3).val, 988000000);
+					assert.same(new unitd.Measure(987654321.01234, undef,  4).val, 987700000);
+					assert.same(new unitd.Measure(987654321.01234, undef,  5).val, 987650000);
+					assert.same(new unitd.Measure(987654321.01234, undef,  6).val, 987654000);
+					assert.same(new unitd.Measure(987654321.01234, undef,  7).val, 987654300);
+					assert.same(new unitd.Measure(987654321.01234, undef,  8).val, 987654320);
+					assert.same(new unitd.Measure(987654321.01234, undef,  9).val, 987654321);
+					assert.same(new unitd.Measure(987654321.01234, undef, 10).val, 987654321.0);
+					assert.same(new unitd.Measure(987654321.01234, undef, 11).val, 987654321.01);
+					assert.same(new unitd.Measure(987654321.01234, undef, 12).val, 987654321.012);
+					assert.same(new unitd.Measure(987654321.01234, undef, 13).val, 987654321.0123);
+					assert.same(new unitd.Measure(987654321.01234, undef, 14).val, 987654321.01234);
+					assert.same(new unitd.Measure(987654321.01234, undef, 15).val, 987654321.01234);
 				},
 				'ctor': {
 					'Measure': function () {
 						var num = new unitd.Measure(3);
-						assert.same(new unitd.Measure(num).raw(), 3);
+						assert.same(new unitd.Measure(num).raw, 3);
 					},
 					'Number': function () {
 						var num = Number(3);
-						assert.same(new unitd.Measure(num).raw(), 3);
+						assert.same(new unitd.Measure(num).raw, 3);
 					},
 					'number': function () {
 						var num = 3;
-						assert.same(new unitd.Measure(num).raw(), 3);
+						assert.same(new unitd.Measure(num).raw, 3);
 					},
 					'string': function () {
 						var num = '3';
-						assert.same(new unitd.Measure(num).raw(), 3);
+						assert.same(new unitd.Measure(num).raw, 3);
 					},
 					'String': function () {
 						var num = String('3');
-						assert.same(new unitd.Measure(num).raw(), 3);
+						assert.same(new unitd.Measure(num).raw, 3);
 					},
 					'Object': function () {
 						var num = { toString: function () { return '3'; } };
-						assert.same(new unitd.Measure(num).raw(), 3);
+						assert.same(new unitd.Measure(num).raw, 3);
 					},
 					'undefined': function () {
 						var num;
@@ -127,15 +127,95 @@
 						    b = new unitd.Measure(5),
 						    sum = a.add(b);
 						assert(sum instanceof unitd.Measure);
-						assert.same(sum.raw(), 8);
+						assert.same(sum.raw, 8);
 					},
 					'chain': function () {
 						var num = new unitd.Measure(4).add(3).multiply(4).divide(2).subtract(1);
-						assert.same(num.val(), 13);
+						assert.same(num.val, 13);
 					},
 					'chained precision': function () {
-						var num = new unitd.Measure(4).add(3).multiply(4).divide(new unitd.Measure(2, 1)).subtract(1);
-						assert.same(num.val(), 10);
+						var num = new unitd.Measure(4).add(3).multiply(4).divide(new unitd.Measure(2, undef, 1)).subtract(1);
+						assert.same(num.val, 10);
+					},
+					'with same units': function () {
+						var a = new unitd.Measure(3, unitd.Unit.Distance.Meter),
+						    b = new unitd.Measure(5, unitd.Unit.Distance.Meter),
+						    sum = a.add(b);
+						assert.same(sum.raw, 8);
+						assert.same(sum.unit, unitd.Unit.Distance.Meter);
+					},
+					'with comparable units': function () {
+						var a = new unitd.Measure(3, unitd.Unit.Distance.Kilometer),
+						    b = new unitd.Measure(5000, unitd.Unit.Distance.Meter),
+						    sum = a.add(b),
+						    communitiveSum = b.add(a);
+						assert.same(sum.raw, 8);
+						assert.same(sum.unit, unitd.Unit.Distance.Kilometer);
+						assert.same(communitiveSum.raw, 8000);
+						assert.same(communitiveSum.unit, unitd.Unit.Distance.Meter);
+					},
+					'with incomparable units': function () {
+						var a = new unitd.Measure(3, unitd.Unit.Distance.Meter),
+						    b = new unitd.Measure(5, unitd.Unit.Amount.Count);
+						try {
+							a.add(b);
+							fail('Exception expected');
+						}
+						catch (e) {
+							assert(e instanceof Error);
+							assert.match(e.message, '');
+						}
+					}
+				}
+			},
+			'units': {
+				'distance': {
+					'': function () {
+						assert(Object.create(unitd.Unit.Distance.prototype) instanceof unitd.Unit);
+
+						assert(unitd.Unit.Distance.Meter.isComparable(unitd.Unit.Distance.Kilometer));
+						assert(unitd.Unit.Distance.Meter.isComparable(unitd.Unit.Distance.Millimeter));
+						assert(unitd.Unit.Distance.Kilometer.isComparable(unitd.Unit.Distance.Meter));
+						assert(unitd.Unit.Distance.Kilometer.isComparable(unitd.Unit.Distance.Millimeter));
+						assert(unitd.Unit.Distance.Millimeter.isComparable(unitd.Unit.Distance.Kilometer));
+						assert(unitd.Unit.Distance.Millimeter.isComparable(unitd.Unit.Distance.Meter));
+
+						refute(unitd.Unit.Distance.Meter.isComparable(unitd.Unit.Amount.Count));
+						refute(unitd.Unit.Distance.Kilometer.isComparable(unitd.Unit.Amount.Count));
+						refute(unitd.Unit.Distance.Millimeter.isComparable(unitd.Unit.Amount.Count));
+						refute(unitd.Unit.Amount.Count.isComparable(unitd.Unit.Distance.Meter));
+						refute(unitd.Unit.Amount.Count.isComparable(unitd.Unit.Distance.Kilometer));
+						refute(unitd.Unit.Amount.Count.isComparable(unitd.Unit.Distance.Millimeter));
+					},
+					'meters': function () {
+						var unit = unitd.Unit.Distance.Meter;
+
+						assert(unit instanceof unitd.Unit);
+						assert(unit instanceof unitd.Unit.Distance);
+
+						assert.same(unit.name, 'Meter');
+						assert.same(unit.abbr, 'm');
+						assert.same(unit.scale, 1);
+					},
+					'kilometers': function () {
+						var unit = unitd.Unit.Distance.Kilometer;
+
+						assert(unit instanceof unitd.Unit);
+						assert(unit instanceof unitd.Unit.Distance);
+
+						assert.same(unit.name, 'Kilometer');
+						assert.same(unit.abbr, 'km');
+						assert.same(unit.scale, 1000);
+					},
+					'millimeters': function () {
+						var unit = unitd.Unit.Distance.Millimeter;
+
+						assert(unit instanceof unitd.Unit);
+						assert(unit instanceof unitd.Unit.Distance);
+
+						assert.same(unit.name, 'Millimeter');
+						assert.same(unit.abbr, 'mm');
+						assert.same(unit.scale, 0.001);
 					}
 				}
 			}
--- unitd.js
+++ unitd.js
@@ -9,10 +9,10 @@
 	'use strict';
 
     // EMCAScript requires at least between 1 and 21 digits of precision.
-    // Some implementations may exceed this range
-    // TODO should we test for precision limits?
-    var MAX_PRECISION = 21,
-        MIN_PRECISION = 1;
+    // We drop the limit to 15 digits to avoid floating point rounding issues.
+    var MAX_PRECISION = 15,
+        MIN_PRECISION = 1,
+        undef;
 
 	define(function () {
 
@@ -40,10 +40,9 @@
             };
         }
 
-        function Measure(value, precision) {
+        function Measure(value, unit, precision) {
             if (value instanceof Measure) {
-                precision = value.precision();
-                value = value.raw();
+                return value;
             }
             else {
                 if (typeof precision !== 'number') {
@@ -62,32 +61,87 @@
                     throw new Error('A number is required, found [' + value + ']');
                 }
 
+                unit = unit || Unit.Reserved.NonUnited;
                 precision = Math.max(Math.min(precision, MAX_PRECISION), MIN_PRECISION);
             }
 
-            this.val = function () {
-                // TODO there's probably a more efficient way to drop precision
-                return Number(value.toPrecision(precision));
-            };
-            this.raw = function () {
-                return value;
-            };
-            this.precision = function () {
-                return precision;
-            };
+            // TODO there's probably a more efficient way to drop precision
+            this.val = Number(value.toPrecision(precision));
+            this.raw = value;
+            this.unit = unit;
+            this.precision = precision;
         }
+
+        function Unit() { throw new Error('Unit is not constructable'); }
+        Object.defineProperty(Unit, 'prototype', { value: Unit.prototype });
+        Unit.register = function register(type, name, abbr, scale) {
+            /*jslint evil: true */
+            var validNameRE = /^[A-Z][a-zA-Z0-9]*$/;
+            if (!validNameRE.test(type)) {
+                throw new Error('Type must be a camel case identifier [' + type + ']');
+            }
+            if (!validNameRE.test(name)) {
+                throw new Error('Name must be a camel case identifier [' + name + ']');
+            }
+
+            if (!Unit[type]) {
+                // define unit type
+                Object.defineProperty(Unit, type, { enumerable: true, value: eval('(function ' + type + '() { throw new Error(\'' + type + ' is not constructable\') })') });
+                Unit[type].prototype = Object.create(Unit.prototype);
+                Object.defineProperties(Unit[type].prototype, {
+                    isComparable: { value: function (other) { return other instanceof Unit[type]; } }
+                });
+                Object.defineProperty(Unit[type], 'property', { value: Unit[type].prototype });
+            }
+            if (Unit[type][name]) {
+                throw new Error('Unit already exists [' + type + '][' + name + ']');
+            }
+
+            // define unit
+            Object.defineProperty(Unit[type], name, { enumerable: true, value: Object.create(Unit[type].prototype) });
+            Object.defineProperties(Unit[type][name], {
+                name: { enumerable: true, value: name },
+                abbr: { enumerable: true, value: abbr },
+                scale: { enumerable: true, value: scale },
+                toString: { value: function () { return name; } }
+            });
+        };
+
 
         var operations = new Operations();
 
         // automatically add operation to Measure instances
-        operations.onRegister(function (name, func) {
-            Measure.prototype[name] = function (measure) {
+        operations.onRegister(function (name, operation) {
+            Measure.prototype[name] = function (measure, unit) {
                 if (!(measure instanceof Measure)) {
-                    measure = new Measure(measure);
+                    measure = new Measure(measure, unit);
                 }
-                return new Measure(func(this.raw(), measure.raw()), Math.min(this.precision(), measure.precision()));
+                if (this.unit === undef && measure.unit === undef) {
+                    // ok
+                    console.log('hello');
+                }
+                else if (this.unit === undef || !this.unit.isComparable(measure.unit)) {
+                    throw new Error('Unit ' + this.unit + ' is not comparable to ' + measure.unit);
+                }
+                return new Measure(
+                    operation(this.raw * this.unit.scale, measure.raw * measure.unit.scale) / this.unit.scale,
+                    this.unit,
+                    Math.min(this.precision, measure.precision)
+                );
             };
         });
+
+        // define basic units
+        Unit.register('Reserved', 'NonUnited', '', 1);
+        Unit.register('Amount', 'Count', '', 1);
+        Unit.register('Distance', 'Kilometer',        'km',         1000      );
+        Unit.register('Distance', 'Meter',            'm',             1      );
+        Unit.register('Distance', 'Millimeter',       'mm',            0.001  );
+        Unit.register('Distance', 'Foot',             'ft',            0.30480);
+        Unit.register('Distance', 'Mile',             'mi',         1609.344  );
+        Unit.register('Distance', 'Yard',             'yd',            0.9144 );
+        Unit.register('Distance', 'Inch',             'in',            0.0254 );
+        Unit.register('Distance', 'AstronomicalUnit', 'au', 149597870700);
 
         // register basic math operations
         operations.register('add',      function (a, b) { return a + b; });
@@ -97,6 +151,7 @@
 
 		return {
             Measure: Measure,
+            Unit: Unit,
             operations: operations
         };
 
