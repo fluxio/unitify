2677f448967878437b4f81b89db244f6447ac397
Scott Andrews <scott@vannevartech.com>
2013-12-12 16:41:01 -0800

    Number and basic operator support
    new unitd.Num(4).add(3).multiply(4).divide(2).subtract(1).val(); // 13

--- test/unitd-test.js
+++ test/unitd-test.js
@@ -14,13 +14,115 @@
 	refute = buster.assertions.refute;
 	fail = buster.assertions.fail;
 
-	define('unitd/unitd-test', function (require) {
+	define('unitd-test', function (require) {
 
 		var unitd = require('unitd');
 
 		buster.testCase('unitd', {
 			'exists': function () {
 				assert(unitd);
+			},
+			'operations': {
+				'add': function () {
+					var add = unitd.operations.getOperations().add;
+					assert.same(add(3, 5), 8);
+					assert.same(add(3, -2), 1);
+				},
+				'subtract': function () {
+					var subtract = unitd.operations.getOperations().subtract;
+					assert.same(subtract(3, 5), -2);
+					assert.same(subtract(3, -2), 5);
+				},
+				'multiply': function () {
+					var multiply = unitd.operations.getOperations().multiply;
+					assert.same(multiply(3, 5), 15);
+					assert.same(multiply(3, -2), -6);
+				},
+				'divide': function () {
+					var divide = unitd.operations.getOperations().divide;
+					assert.same(divide(3, 5), 0.6);
+					assert.same(divide(3, -2), -1.5);
+				}
+			},
+			'Num': {
+				'val': function () {
+					assert.same(new unitd.Num(3).val(), 3);
+				},
+				'ctor': {
+					'Num': function () {
+						var num = new unitd.Num(3);
+						assert.same(new unitd.Num(num).val(), 3);
+					},
+					'Number': function () {
+						var num = Number(3);
+						assert.same(new unitd.Num(num).val(), 3);
+					},
+					'number': function () {
+						var num = 3;
+						assert.same(new unitd.Num(num).val(), 3);
+					},
+					'string': function () {
+						var num = '3';
+						assert.same(new unitd.Num(num).val(), 3);
+					},
+					'String': function () {
+						var num = String('3');
+						assert.same(new unitd.Num(num).val(), 3);
+					},
+					'Object': function () {
+						var num = { toString: function () { return '3'; } };
+						assert.same(new unitd.Num(num).val(), 3);
+					},
+					'undefined': function () {
+						var num;
+						try {
+							num = new unitd.Num(num);
+							fail('Error expected');
+						}
+						catch (e) {
+							assert(e instanceof Error);
+							assert.match(e.message, 'A number is required');
+						}
+					},
+					'NaN': function () {
+						var num = NaN;
+						try {
+							num = new unitd.Num(num);
+							fail('Error expected');
+						}
+						catch (e) {
+							assert(e instanceof Error);
+							assert.match(e.message, 'A number is required');
+						}
+					}
+				},
+				'operation': {
+					'with Num': function () {
+						var a = new unitd.Num(3),
+						    b = new unitd.Num(5),
+						    sum = a.add(b);
+						assert(sum instanceof unitd.Num);
+						assert.same(sum.val(), 8);
+					},
+					'with primative': function () {
+						var a = new unitd.Num(3),
+						    b = 5,
+						    sum = a.add(b);
+						assert(sum instanceof unitd.Num);
+						assert.same(sum.val(), 8);
+					},
+					'with Number': function () {
+						var a = new unitd.Num(3),
+						    b = Number(5),
+						    sum = a.add(b);
+						assert(sum instanceof unitd.Num);
+						assert.same(sum.val(), 8);
+					},
+					'chain': function () {
+						var num = new unitd.Num(4).add(3).multiply(4).divide(2).subtract(1);
+						assert.same(num.val(), 13);
+					}
+				}
 			}
 		});
 
--- unitd.js
+++ unitd.js
@@ -10,7 +10,69 @@
 
 	define(function () {
 
-		return {};
+        function Operations() {
+            var ops = {},
+                listeners = [];
+
+            this.register = function register(name, func) {
+                if (ops.hasOwnProperty(name)) {
+                    throw new Error('Operation [' + name + '] is already registered');
+                }
+                ops[name] = func;
+                listeners.forEach(function (listener) {
+                    listener(name, func);
+                });
+            };
+
+            this.onRegister = function onRegister(listener) {
+                listeners.push(listener);
+            };
+
+            this.getOperations = function getOperations() {
+                // TODO clone to protect from side effects
+                return ops;
+            };
+        }
+
+        function Num(value) {
+            if (value instanceof Num) {
+                value = value.val();
+            }
+            else if (typeof value !== 'number') {
+                value = Number(value).valueOf();
+            }
+            if (isNaN(value)) {
+                // TODO use non-coerced value in error message
+                throw new Error('A number is required, found [' + value + ']');
+            }
+
+            this.val = function val() {
+                return value;
+            };
+        }
+
+        var operations = new Operations();
+
+        // automatically add operation to Num instances
+        operations.onRegister(function (name, func) {
+            Num.prototype[name] = function (num) {
+                if (!(num instanceof Num)) {
+                    num = new Num(num);
+                }
+                return new Num(func(this.val(), num.val()));
+            };
+        });
+
+        // register basic math operations
+        operations.register('add',      function (a, b) { return a + b; });
+        operations.register('subtract', function (a, b) { return a - b; });
+        operations.register('multiply', function (a, b) { return a * b; });
+        operations.register('divide',   function (a, b) { return a / b; });
+
+		return {
+            Num: Num,
+            operations: operations
+        };
 
 	});
 
